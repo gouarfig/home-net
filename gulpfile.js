d6d17122bfe82f03b3729cd0c7401f19f2ba1cb8
Fred Quointeau <fred.quointeau@live.com>
2016-08-04 16:11:49 +0100

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    cssmin = require('gulp-cssmin'),
+    uglify = require('gulp-uglify');
+
+gulp.task('default', function() {
+
+});
