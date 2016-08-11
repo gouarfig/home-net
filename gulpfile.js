0bfcc0aabcedccee8416c2aba3fef8be6959b8dd
Frederic Quointeau <gouarfig@Ethelwyn.local.net>
2016-08-11 18:16:27 +0100

    Added clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     cssmin = require('gulp-cssmin'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    del = require('del');
 
 gulp.task('default', function() {
 
+});
+
+gulp.task('clean', ['clean:js']);
+
+gulp.task('clean:js', function() {
+    return del([
+        'app/*.js',
+        'app/*.js.map',
+    ]);
 });
