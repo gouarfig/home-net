9db027b3775bcf72a8a115b9e89c03489a75a199
Fred Quointeau <fred.quointeau@live.com>
2016-08-16 15:49:54 +0100

    Updated SQL schema

--- sql/homenet.sql
+++ sql/homenet.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 16, 2016 at 02:59 PM
+-- Generation Time: Aug 16, 2016 at 03:47 PM
 -- Server version: 5.5.50
 -- PHP Version: 5.4.45-0+deb7u4
 
@@ -27,20 +27,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `weather` (
-  `weather_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `weather` varchar(100) NOT NULL,
-  `temperature` smallint(5) NOT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `weather_id` smallint(5) unsigned NOT NULL,
+  `temperature` decimal(5,2) NOT NULL,
   `pressure` smallint(5) unsigned NOT NULL,
   `humidity` smallint(5) unsigned NOT NULL,
-  `wind_speed` smallint(5) unsigned NOT NULL,
-  `gust_speed` smallint(5) unsigned NOT NULL,
+  `wind_speed` decimal(5,2) unsigned NOT NULL,
+  `gust_speed` decimal(5,2) unsigned NOT NULL,
   `wind_direction` smallint(5) unsigned NOT NULL,
-  `recorded` datetime NOT NULL,
-  PRIMARY KEY (`weather_id`),
-  UNIQUE KEY `recorded` (`recorded`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `clouds` smallint(5) unsigned NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `recorded` (`updated`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */; 
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
