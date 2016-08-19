4939b4bbd56b0cc5e541f810448c97b8ebad8560
Fred Quointeau <fred.quointeau@live.com>
2016-08-19 10:20:45 +0100

    New DB structure

--- sql/homenet.sql
+++ sql/homenet.sql
@@ -1,34 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 3.4.11.1deb2+deb7u5
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Aug 16, 2016 at 03:47 PM
--- Server version: 5.5.50
--- PHP Version: 5.4.45-0+deb7u4
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `homenet`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `weather`
---
 
 CREATE TABLE IF NOT EXISTS `weather` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `weather_id` smallint(5) unsigned NOT NULL,
+  `weather_type_id` smallint(5) unsigned NOT NULL,
   `temperature` decimal(5,2) NOT NULL,
   `pressure` smallint(5) unsigned NOT NULL,
   `humidity` smallint(5) unsigned NOT NULL,
@@ -39,9 +20,16 @@ CREATE TABLE IF NOT EXISTS `weather` (
   `updated` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `recorded` (`updated`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `weather_type` (
+  `weather_type_id` smallint(5) unsigned NOT NULL,
+  `main` varchar(50) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `icon` varchar(20) NOT NULL,
+  PRIMARY KEY (`weather_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
