fcb912fba1a6085cc7a815981b910cd8a92fe40c
Frederic Quointeau <gouarfig@Ethelwyn.local.net>
2016-08-21 00:15:16 +0100

    Added simple history component

---
+++ app/history.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Jsonp, JSONP_PROVIDERS } from '@angular/http';
+import { CHART_DIRECTIVES } from 'angular2-highcharts';
+ 
+ @Component({
+    selector: 'weather-history',
+    directives: [CHART_DIRECTIVES],
+    providers: [JSONP_PROVIDERS],
+    template: `<chart type="StockChart" [options]="options"></chart>`
+})
+export class WeatherHistory {
+    constructor(jsonp : Jsonp) {
+        jsonp.request('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=JSONP_CALLBACK').subscribe(res => {
+            this.options = {
+                credits: { enabled: false },
+                title : { text : 'AAPL Stock Price' },
+                series : [{
+                    name : 'AAPL',
+                    data : res.json(),
+                    tooltip: {
+                        valueDecimals: 2
+                    }
+                }]
+            };
+        });
+    }
+    options: Object;
+}
--- app/weatherpage.component.ts
+++ app/weatherpage.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { WeatherComponent } from './weather.component';
+import { WeatherHistory } from './history.component';
 
 @Component({
   selector: 'weatherpage',
   templateUrl: 'app/weatherpage.component.html',
   directives: [
-    WeatherComponent
+    WeatherComponent,
+    WeatherHistory
     ]
 })
 export class WeatherPageComponent { }
